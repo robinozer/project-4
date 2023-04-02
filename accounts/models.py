d6b9adb7804c255fefd39ba6e675425326e31a63
robinozer <robin.ozer@outlook.com>
2023-04-02 17:37:55 +0000

    moved booking app models to accounts app models.py file  since one app is easier to manage for this project

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, User
 
 
 class UserManager(BaseUserManager):
@@ -44,6 +44,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30, blank=False, null=False)
     last_name = models.CharField(max_length=30, blank=False, null=False)
+    # is_active is used manage user account status and permissions in the app
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -77,3 +78,28 @@ class User(AbstractBaseUser):
         if app_label == 'auth':
             return self.is_superuser
         return True
+
+
+class Table(models.Model):
+    """A model representing a table in the restaurant."""
+    number = models.IntegerField()  # The table number
+    capacity = models.IntegerField()  # Max no of guests the table can seat
+
+    def __str__(self):
+        """Return a string representation of the table."""
+        return f'Table {self.number} with capacity of {self.capacity} guests.'
+
+
+class Booking(models.Model):
+    """A model representing a booking made by a user."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # The user who made the booking
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)  # The table reserved by the user
+    date_time = models.DateTimeField()  # The date and time of the booking
+    guests = models.IntegerField()  # The number of guests for the booking
+
+    class Meta:
+        ordering = ['-date_time']
+
+    def __str__(self):
+        """Return a string representation of the booking."""
+        return f'{self.user.username} has booked - table no. {self.table} - {self.date_time}, for {self.guests} guests.'
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from .models import Reservation
+from .forms import ReservationForm
+
+@login_required
+def reservations(request):
+    reservations = Reservation.objects.filter(user=request.user)
+    return render(request, 'bookings/reservations.html', {'reservations': reservations})
+
+
