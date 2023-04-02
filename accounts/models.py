44933f486b6f1ab695470460a1bd7ecb6c29d435
robinozer <robin.ozer@outlook.com>
2023-04-02 14:08:39 +0000

    add more comments to explain usermanager model and format some rows

--- accounts/models.py
+++ accounts/models.py
@@ -5,34 +5,55 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 class UserManager(BaseUserManager):
     # Define a model that allows for custom user and superuser creation
     def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a new User with the given email and password.
+        """
         if not email:
             raise ValueError("Email address is required")
 
+        # Normalize the email address to lower case
         email = self.normalize_email(email)
+
+        # Create a new User instance with the given email and any extra fields
         user = self.model(email=email, **extra_fields)
+
+        # Set the user's password using the set_password method, which
+        # hashes the password before saving it
         user.set_password(password)
+
+        # Save the user to the database using the database connection
+        # used by the manager
         user.save(using=self._db)
+
+        # Return the newly created user instance
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a new superuser with the given email and password.
+        """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
+        # Call create_user to create the new superuser
         return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser):
     # Define fields for the User model
-    email = models.EmailField(unique=True)  # User's email address
-    first_name = models.CharField(max_length=30, blank=True)  # User's first name
-    last_name = models.CharField(max_length=30, blank=True)  # User's last name
-    is_active = models.BooleanField(default=True)  # Whether the user is active or not
-    is_staff = models.BooleanField(default=False)  # Whether the user is a staff member or not
-    is_superuser = models.BooleanField(default=False)  # Whether the user is a superuser or not
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
-    # Define the objects manager for the User model to handle user creation and management
+    # Define the objects manager for the User model
+    # to handle user creation and management
     objects = UserManager()
 
-    # Set the USERNAME_FIELD to email so that users can log in with their email address
+    # Set the USERNAME_FIELD to email so that
+    # users can log in with their email address
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
@@ -50,7 +71,8 @@ class User(AbstractBaseUser):
     def has_perm(self, perm, obj=None):
         return True
 
-    # Define a method to check if the user has permissions for a specific module
+    # Define a method to check if the user
+    # has permissions for a specific module
     def has_module_perms(self, app_label):
         if app_label == 'auth':
             return self.is_superuser
