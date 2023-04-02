2307c32bd07bf341313efeaa50adc7ed436b2894
robinozer <robin.ozer@outlook.com>
2023-04-02 13:27:28 +0000

    add usermanager model, normalize email to prevent multiple signups with same email

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email address is required")
+
+    email = self.normalize_email(email)
