f8b9172d9c34d030856a26b7706edf0a90f2b9f7
robinozer <robin.ozer@outlook.com>
2023-04-02 13:07:19 +0000

    add model for booking with info on user, date, time, table and guest number

--- booking/models.py
+++ booking/models.py
@@ -9,4 +9,16 @@ class Table(models.Model):
 
     def __str__(self):
         """Return a string representation of the table."""
-        return f'Table {self.number}'
+        return f'Table {self.number} with capacity of {self.capacity} guests.'
+
+class Booking(models.Model):
+    """A model representing a booking made by a user."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # The user who made the booking
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)  # The table reserved by the user
+    date = models.DateField()  # The date of the booking
+    time = models.TimeField()  # The time of the booking
+    guests = models.IntegerField()  # The number of guests for the booking
+
+    def __str__(self):
+        """Return a string representation of the booking."""
+        return f'{self.user.username} has booked - table no. {self.table} - for {self.date} {self.time}, for {self.guests} guests.'
