edaba772c71fa7b70e9e46e3bdb356645d328750
robinozer <robin.ozer@outlook.com>
2023-04-02 14:11:05 +0000

    make first and last name fields mandatory for the User model

--- accounts/models.py
+++ accounts/models.py
@@ -42,8 +42,8 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser):
     # Define fields for the User model
     email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=30, blank=True)
+    first_name = models.CharField(max_length=30, blank=False, null=False)
+    last_name = models.CharField(max_length=30, blank=False, null=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
