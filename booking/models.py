ed0c832eccbdcd5d3e59a092fa842b00f57e636a
robinozer <robin.ozer@outlook.com>
2023-04-02 14:25:51 +0000

    import models to admin

--- booking/models.py
+++ booking/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Table(models.Model):
     """A model representing a table in the restaurant."""
     number = models.IntegerField()  # The table number
-    capacity = models.IntegerField()  # The maximum number of guests the table can seat
+    capacity = models.IntegerField()  # Max no of guests the table can seat
 
     def __str__(self):
         """Return a string representation of the table."""
