69bdcc7159cae43401441371df35d352e98995c2
robinozer <robin.ozer@outlook.com>
2023-04-02 12:30:05 +0000

    add table model for admin view

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Table(models.Model):
+    """A model representing a table in the restaurant."""
+    number = models.IntegerField()  # The table number
+    capacity = models.IntegerField()  # The maximum number of guests the table can seat
+
+    def __str__(self):
+        """Return a string representation of the table."""
+        return f'Table {self.number}'
